c637bf4588d71b655b0b27c38258e15d6fc3249f
CyrilSharma <sharma.cyril@gmail.com>
2023-04-07 16:12:31 -0400

    add setup.sh

---
+++ setup.sh
@@ -0,0 +1,4 @@
+sudo apt-get update
+sudo apt-get update && sudo apt-get install git nasm build-essential qemu gdb
+sudo apt-get install -y qemu-kvm libvirt-daemon-system libguestfs-tools bridge-utils
+sudo apt-get install -y libgtk2.0-dev
